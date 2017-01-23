6ad600eeb651c3a9a4c77b66189530e6cd2ee243
Ben Hong <bencodezen@gmail.com>
2017-01-23 17:18:15 -0500

    chore: Update gulp default task to process CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 
-gulp.task('default', function() {
-
-})
-
 gulp.task('css', function() {
 	return gulp.src('./src/scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./src/css'))
 })
+
+gulp.task('default', ['css'])
